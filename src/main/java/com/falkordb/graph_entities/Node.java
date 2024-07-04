78d2b6bfdbce00b0e151f5d0315d8c5370d0311a
yangdingming <yangdingming@ut.cn>
2024-07-04 15:47:48 +0800

    optimize Label structure

--- src/main/java/com/falkordb/graph_entities/Node.java
+++ src/main/java/com/falkordb/graph_entities/Node.java
@@ -1,8 +1,6 @@
 package com.falkordb.graph_entities;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * * A class represent an node (graph entity). In addition to the base class id and properties, a node has labels.
@@ -10,22 +8,23 @@ import java.util.Objects;
 public class Node extends GraphEntity {
 
     //members
-    private final List<String> labels;
+    private final Set<String> labels;
 
     public Node() {
         super();
-        labels = new ArrayList<>();
+        labels = new HashSet<>();
     }
 
     /**
-     * Use this constructor to reduce memory allocations 
+     * Use this constructor to reduce memory allocations
      * when labels or properties are added to the node
-     * @param labelsCapacity preallocate the capacity for the node labels
+     *
+     * @param labelsCapacity     preallocate the capacity for the node labels
      * @param propertiesCapacity preallocate the capacity for the properties
      */
     public Node(int labelsCapacity, int propertiesCapacity) {
         super(propertiesCapacity);
-        this.labels = new ArrayList<>(labelsCapacity);
+        this.labels = new HashSet<>(labelsCapacity);
     }
 
 
@@ -44,17 +43,25 @@ public class Node extends GraphEntity {
     }
 
     /**
-     * @param index - label index
-     * @return the property label
-     * @throws IndexOutOfBoundsException if the index is out of range
-     *                                   ({@code index < 0 || index >= getNumberOfLabels()})
+     *
+     * @param label
+     * @return
      */
-    public String getLabel(int index){
-        return labels.get(index);
+    public boolean hasLabel(String label) {
+        return labels.contains(label);
     }
 
+//    /**
+//     * @param index - label index
+//     * @return the property label
+//     * @throws IndexOutOfBoundsException if the index is out of range
+//     *                                   ({@code index < 0 || index >= getNumberOfLabels()})
+//     */
+//    public String getLabel(int index) {
+//        return labels.get(index);
+//    }
+
     /**
-     *
      * @return the number of labels
      */
     public int getNumberOfLabels() {
